8f679089e680c73c92bef064bb2ab10460850b66
rlorenzo <raphael.lorenzo@inria.fr>
2026-08-28 20:13:25 +0200

    fix

--- ros2_node_3d_pose.py
+++ ros2_node_3d_pose.py
@@ -296,7 +296,10 @@ class Pose3DNode(Node):
     def _process_frame(self, bgr: np.ndarray, depth_raw: np.ndarray, stamp):
         t0 = time.perf_counter()
         results = self.pose_model.track(bgr, persist=True, verbose=False, tracker="bytetrack.yaml")[0]
-        self.get_logger().info(f"Got {len(results.boxes.id)} tracks")
+        if results.boxes.id is None:
+            self.get_logger().warning("No tracks found")
+        else:
+            self.get_logger().info(f"Got {len(results.boxes.id)} tracks")
 
         marker_array = MarkerArray()
         clear = Marker()
